d1811b673298e7315453d9508d3fdcebf4f76ee6
睿宁 <ruining@bestheme.ac.cn>
2022-10-09 19:47:26 +0800

    增加流月干和支方法

--- Sources/SwiftLunar/Timeset/LiuYue.swift
+++ Sources/SwiftLunar/Timeset/LiuYue.swift
@@ -36,6 +36,27 @@ public struct LiuYue {
         return gan + zhi;
     }
     
+    public func getGan() -> String {
+        var offset: Int = 0;
+        let yearGan: String = String(liuNian.getGanZhi().prefix(1));
+        if ("甲" == yearGan || "己" == yearGan) {
+            offset = 2
+        } else if ("乙" == yearGan || "庚" == yearGan) {
+            offset = 4
+        } else if ("丙" == yearGan || "辛" == yearGan) {
+            offset = 6
+        } else if ("丁" == yearGan || "壬" == yearGan) {
+            offset = 8
+        }
+        let gan: String = LunarUtil.GAN[(index + offset) % 10 + 1];
+        return gan;
+    }
+    
+    public func getZhi() -> String {
+        let zhi: String = LunarUtil.ZHI[(index + LunarUtil.BASE_MONTH_ZHI_INDEX) % 12 + 1];
+        return zhi;
+    }
+    
     public func getXun() -> String {
         return LunarUtil.getXun(ganZhi: getGanZhi())
     }
